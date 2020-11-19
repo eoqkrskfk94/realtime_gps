4b23221f01fc7c7efaec99f9796371b27ec8e2e0
김명진 <mobinity.kmj@gmail.com>
2020-11-19 15:21:23 +0900

    실시간 gps 기능 구현

---
+++ app/src/main/java/com/example/realtime_gps/GpsService.kt
@@ -0,0 +1,124 @@
+package com.example.realtime_gps
+
+import android.Manifest
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.app.Service
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
+import android.os.IBinder
+import android.os.Looper
+import android.util.Log
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationCompat
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
+import java.lang.UnsupportedOperationException
+
+
+class GpsService : Service() {
+
+    private val locationCallBack: LocationCallback? = object : LocationCallback() {
+        override fun onLocationResult(locationResult: LocationResult?) {
+            super.onLocationResult(locationResult)
+
+            if(locationResult != null && locationResult.lastLocation != null){
+                var latitude = locationResult.lastLocation.latitude
+                var longitude = locationResult.lastLocation.longitude
+                Log.d("LOCATION_UPDATE", "위도: $latitude, 경도: $longitude")
+            }
+
+        }
+
+    }
+
+    override fun onBind(p0: Intent?): IBinder? {
+        throw UnsupportedOperationException("Not yet implemented")
+    }
+
+    override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int {
+        Toast.makeText(this, "Service started by user.", Toast.LENGTH_LONG).show()
+
+        if(intent != null){
+            val action = intent.action
+            if(action != null){
+                if(action == Constants.ACTION_START_LOCATION_SERVICE){
+                    println("start")
+                    startLocationService()
+                }
+                else if(action == Constants.ACTION_STOP_LOCATION_SERVICE){
+                    println("stop")
+                    stopLocationService()
+                }
+            }
+        }
+
+
+        return super.onStartCommand(intent, flags, startId)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Toast.makeText(this, "Service destroyed by user.", Toast.LENGTH_LONG).show()
+
+    }
+
+
+
+    private fun startLocationService(){
+        val channelId = "location_notification_channel"
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val resultIntent = Intent(applicationContext, MainActivity::class.java)
+        val pendingIntent = PendingIntent.getActivity(applicationContext,0, resultIntent, PendingIntent.FLAG_UPDATE_CURRENT)
+
+        val builder = NotificationCompat.Builder(applicationContext, channelId)
+
+        builder.setSmallIcon(R.mipmap.ic_launcher)
+        builder.setContentTitle("두딜 앱 실행중입니다.")
+        builder.setDefaults(NotificationCompat.DEFAULT_ALL)
+        builder.setContentText("실시간 GPS 작동중...")
+        builder.setContentIntent(pendingIntent)
+        builder.setAutoCancel(false)
+        builder.setPriority(NotificationCompat.PRIORITY_MAX)
+
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            if(notificationManager != null && notificationManager.getNotificationChannel(channelId) == null){
+                val notificationChannel = NotificationChannel(channelId, "Location Service", NotificationManager.IMPORTANCE_HIGH)
+                notificationChannel.description = "This channel is used by location service"
+                notificationManager.createNotificationChannel(notificationChannel)
+            }
+        }
+
+        val locationRequest = LocationRequest()
+        locationRequest.interval = 5000
+        locationRequest.fastestInterval = 2000
+        locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+            && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED
+        ) {
+            LocationServices.getFusedLocationProviderClient(this)
+                .requestLocationUpdates(locationRequest, locationCallBack, Looper.getMainLooper())
+            startForeground(Constants.LOCATION_SERVICE_ID, builder.build())
+        }
+
+    }
+
+
+    private fun stopLocationService(){
+        LocationServices.getFusedLocationProviderClient(this)
+            .removeLocationUpdates(locationCallBack)
+        stopForeground(true)
+        stopSelf()
+
+    }
+
+
+}
--- app/src/main/java/com/example/realtime_gps/MainActivity.kt
+++ app/src/main/java/com/example/realtime_gps/MainActivity.kt
@@ -2,29 +2,24 @@
 package com.example.realtime_gps
 
 import android.Manifest
-import android.app.Notification
+import android.app.ActivityManager
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
-import android.graphics.BitmapFactory
-import android.graphics.Color
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
-import androidx.appcompat.app.AlertDialog
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import androidx.core.content.ContextCompat
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationCallback
-import com.google.android.gms.location.LocationRequest
-import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.*
 import com.google.android.libraries.maps.model.LatLng
 
 
@@ -34,7 +29,9 @@ class MainActivity : AppCompatActivity() {
     private val NOTIFICATION_ID = 1001
     private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
     private lateinit var locationRequest: LocationRequest
-    private lateinit var locationCallback: MyLocationCallBack
+    //private lateinit var locationCallback: MyLocationCallBack
+
+    private lateinit var notificationManager: NotificationManagerCompat
 
 
 
@@ -43,58 +40,51 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         getPermission()
-        locationInit()
+        //locationInit()
 
-        val btn: Button = findViewById(R.id.btn)
-
-
-        createNotificationChannel(this, NotificationManagerCompat.IMPORTANCE_DEFAULT,
-            false, getString(R.string.app_name), "App notification channel") // 1
-
-
-        val channelId = "$packageName-${getString(R.string.app_name)}" // 2
-        val title = "Android Developer"
-        val content = "Notifications in Android P"
-
-
-        val intent = Intent(baseContext, MainActivity::class.java)
-        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-        val pendingIntent = PendingIntent.getActivity(baseContext, 0,
-            intent, PendingIntent.FLAG_UPDATE_CURRENT)    // 3
-
-        val builder = NotificationCompat.Builder(this, channelId)  // 4
-        builder.setSmallIcon(R.drawable.ic_launcher_background)    // 5
-        builder.setContentTitle(title)    // 6
-        builder.setContentText(content)    // 7
-        builder.priority = NotificationCompat.PRIORITY_MAX    // 8
-        builder.setAutoCancel(true)   // 9
-        builder.setOngoing(true)
-        builder.setContentIntent(pendingIntent)   // 10
+        val btnStart: Button = findViewById(R.id.btn_start)
+        val btnStop: Button = findViewById(R.id.btn_stop)
 
 
 
 
-        btn.setOnClickListener {
-            val notificationManager = NotificationManagerCompat.from(this)
-            notificationManager.notify(NOTIFICATION_ID, builder.build())    // 11
+
+
+
+
+        btnStart.setOnClickListener {
+            startLocationService()
+        }
+
+        btnStop.setOnClickListener {
+            stopLocationService()
         }
 
 
 
     }
 
+//    private fun isLocationServiceRunning(){
+//        val activityManager = getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
+//        if(activityManager != null){
+//            for (service in activityManager.getRunningServices(Int.MAX_VALUE)){
+//                if(LocationService.class)
+//            }
+//        }
+//    }
 
-    private fun createNotificationChannel(context: Context, importance: Int, showBadge: Boolean,
-                                          name: String, description: String) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val channelId = "${context.packageName}-$name"
-            val channel = NotificationChannel(channelId, name, importance)
-            channel.description = description
-            channel.setShowBadge(showBadge)
+    private fun startLocationService(){
+        val intent = Intent(applicationContext, GpsService::class.java)
+        intent.action = Constants.ACTION_START_LOCATION_SERVICE
+        startService(intent)
+        Toast.makeText(this, "Location service started", Toast.LENGTH_SHORT).show()
+    }
 
-            val notificationManager = context.getSystemService(NotificationManager::class.java)
-            notificationManager.createNotificationChannel(channel)
-        }
+    private fun stopLocationService(){
+        val intent = Intent(applicationContext, GpsService::class.java)
+        intent.action = Constants.ACTION_STOP_LOCATION_SERVICE
+        startService(intent)
+        Toast.makeText(this, "Location service stopped", Toast.LENGTH_SHORT).show()
     }
 
 
@@ -102,59 +92,74 @@ class MainActivity : AppCompatActivity() {
 
 
 
-    fun locationInit(){
-        fusedLocationProviderClient = FusedLocationProviderClient(this)
-
-        locationRequest = LocationRequest()
-
-        locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
-        locationRequest.interval = 5000
-        locationRequest.fastestInterval = 1000
-
-        locationCallback = MyLocationCallBack()
-
-    }
-
-    fun addLocationListener(){
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            return
-        }
-        fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, null)
-    }
-
-    inner class MyLocationCallBack: LocationCallback(){
-        override fun onLocationResult(locationResult: LocationResult?) {
-            super.onLocationResult(locationResult)
-
-            val location = locationResult?.lastLocation
-
-            location?.run{
-                val latLng = LatLng(latitude, longitude)
-
-                Log.d("MainActivity", "위도: $latitude, 경도: $longitude")
-            }
-        }
-
-    }
+//    fun locationInit(){
+//        fusedLocationProviderClient = FusedLocationProviderClient(this)
+//
+//        locationRequest = LocationRequest()
+//
+//        locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+//        locationRequest.interval = 5000
+//        locationRequest.fastestInterval = 1000
+//
+//        locationCallback = MyLocationCallBack()
+//
+//    }
+//
+//    fun addLocationListener(){
+//        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+//                this,
+//                Manifest.permission.ACCESS_COARSE_LOCATION
+//            ) != PackageManager.PERMISSION_GRANTED) {
+//            return
+//        }
+//        fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, null)
+//    }
+//
+//    inner class MyLocationCallBack: LocationCallback(){
+//        override fun onLocationResult(locationResult: LocationResult?) {
+//            super.onLocationResult(locationResult)
+//
+//            val location = locationResult?.lastLocation
+//
+//            location?.run{
+//                val latLng = LatLng(latitude, longitude)
+//
+//                Log.d("MainActivity", "위도: $latitude, 경도: $longitude")
+//            }
+//        }
+//
+//    }
 
     override fun onResume() {
         super.onResume()
-
-        addLocationListener()
+        //addLocationListener()
     }
 
 
     private fun getPermission(){
-        if(ContextCompat.checkSelfPermission(this,
-                        android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+        if(ContextCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED){
 
-            if(ActivityCompat.shouldShowRequestPermissionRationale(this, android.Manifest.permission.ACCESS_FINE_LOCATION)){
+            if(ActivityCompat.shouldShowRequestPermissionRationale(
+                    this,
+                    android.Manifest.permission.ACCESS_FINE_LOCATION
+                )){
 
 
             }
             else{
-                ActivityCompat.requestPermissions(this@MainActivity, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), REQUEST_FINE_LOCATION)
-                ActivityCompat.requestPermissions(this@MainActivity, arrayOf(android.Manifest.permission.ACCESS_COARSE_LOCATION), REQUEST_FINE_LOCATION)
+                ActivityCompat.requestPermissions(
+                    this@MainActivity,
+                    arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),
+                    REQUEST_FINE_LOCATION
+                )
+                ActivityCompat.requestPermissions(
+                    this@MainActivity,
+                    arrayOf(android.Manifest.permission.ACCESS_COARSE_LOCATION),
+                    REQUEST_FINE_LOCATION
+                )
             }
         }
     }
